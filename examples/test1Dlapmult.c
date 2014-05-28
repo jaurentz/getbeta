c27c46b2154aee996ab859260c6826a4e3e18577
jminor07 <johnminor07@gmail.com>
2014-05-27 17:14:50 -0700

    added void function

--- examples/test1Dlapmult.c
+++ examples/test1Dlapmult.c
@@ -2,53 +2,59 @@
 #include <stdlib.h>
 #include <math.h>
 
+void lapmult(int n,double*X,double*Y);
+
 int main(){
 
-	// print hello world
-	printf("\nhello world!\n\n");
-
-	// declare a variable
-	int myage = 13;
-	
-	// print myage
-	printf("\nmyage = %d\n\n",myage);
-	
-	// declare a double
-	double mydouble = (double)5/2;
-	
-	// print mydouble
-	printf("\nmydouble = %1.15e\n\n",mydouble);
 	
 	// allocate dynamic memory
-	int n = 20;
-	double *myvec; //pointer to double array
-	myvec = (double*)malloc(n*sizeof(double));
-	
-	// for loop to print myvec
+	int n = 10;
+	double *X; //pointer to double array
+	X = (double*)malloc(n*sizeof(double));
+
+	double *Y; //pointer to double array
+	Y = (double*)malloc(n*sizeof(double));
+
+	// for loop to change X
 	int ii;
-	printf("\nmyvec:\n");
 	for(ii=0;ii<n;ii++){
-		printf("myvec[%d] = %+1.15e\n",ii,myvec[ii]);
+		X[ii] = 2;
+	}
+	//for loop to change Y
+	for(ii=0;ii<n;ii++){
+		Y[ii] = 0;
+	}
+
+	lapmult(n,X,Y);
+
+	// for loop to print Y
+	
+	printf("\nThis is Y:\n");
+	for(ii=0;ii<n;ii++){
+		printf("Y[%d] = %+1.15e\n",ii,Y[ii]);
 	}
 	printf("\n");
-	
-	// for loop to change myvec
-	for(ii=0;ii<n;ii++){
-		myvec[ii] = ii*ii;
-	}
-	
-	// for loop to print myvec
-	printf("\nmyvec:\n");
-	for(ii=0;ii<n;ii+=2){
-		printf("myvec[%d] = %+1.15e\n",ii,myvec[ii]);
-	}
-	printf("\n");
-	
+
 	// free memory
-	free(myvec);
+	free(X);
+	free(Y);
 
 	// return value
 	return 0;
 
-};
+}
 
+
+void lapmult(int n,double*X,double*Y){
+
+	int ii;
+	//computing Y
+	for(ii=1;ii<n-1;ii++){
+		Y[ii] = -X[ii-1]+2*X[ii]-X[ii+1];
+	}
+	//top row of laplacian 
+	Y[0] = 2*X[0]-X[1];
+	
+	//bottom row of laplacian 
+	Y[n-1] = 2*X[n-1]-X[n-2];
+}
