52687e21f4515bfb372a892db81f5ac8b80cfa60
jaurentz <jaurentz@math.wsu.edu>
2014-06-20 14:05:04 -0700

    added cublas to testcucheb.cu

--- examples/testcucheb.cu
+++ examples/testcucheb.cu
@@ -31,6 +31,30 @@ int main(void){
 	// set device memory using CUCHEB
 	cuchebCheckError(cuchebDinit(n,dx,1,1.0),__FILE__,__LINE__);
 	cuchebCheckError(cuchebDinit(n,dy,1,0.0),__FILE__,__LINE__);
+
+	// create cublas handle
+	cublasHandle_t handle;
+	cuchebCheckError(cublasCreate(&handle),__FILE__,__LINE__);
+	
+	// set pointer mode to host
+	cuchebCheckError(cublasSetPointerMode(handle, CUBLAS_POINTER_MODE_HOST),__FILE__,__LINE__);
+
+	// compute norm of dx
+	double nrmx;
+	cuchebCheckError(cublasDnrm2(handle, n, dx, 1, &nrmx),__FILE__,__LINE__);
+
+	// print norm of x
+	printf("\nnorm of x = %+1.15e\n\n",nrmx);
+
+	// compute norm of dy
+	double nrmy;
+	cuchebCheckError(cublasDnrm2(handle, n, dy, 1, &nrmy),__FILE__,__LINE__);
+
+	// print norm of y
+	printf("\nnorm of y = %+1.15e\n\n",nrmy);
+
+	// destroy cublas handle
+	cuchebCheckError(cublasDestroy(handle),__FILE__,__LINE__);
 	
 	// copy device memory to host
 	cuchebCheckError(cudaMemcpy(x,dx,n*sizeof(double),cudaMemcpyDeviceToHost),__FILE__,__LINE__);
